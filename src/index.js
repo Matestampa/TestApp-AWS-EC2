46dcdf9b1cd67d9cfdba7319fe8e28a2c8fcde6e
Matestampa <paredpiso5@gmail.com>
2024-03-10 20:34:33 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const App=express();
+
+const IP = require('ip');
+
+
+App.use(express.json());
+
+
+App.get("/",(req,res)=>{
+    console.log(req);
+    let publicIp=req.ip;
+    let publicIpFor=req.header["x-forwarde-for"];
+
+
+    res.send(`Your ip is ${publicIp}`);
+})
+
+
+const PORT=3000;
+
+App.listen(PORT,()=>{
+    console.log(`Server running on port:${PORT}`)
+});
