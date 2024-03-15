fbb749bbaf350a1a0cde978432f568945c2d3b1f
Matestampa <paredpiso5@gmail.com>
2024-03-15 20:08:23 -0300

    Support diferent env files

--- src/config.js
+++ src/config.js
@@ -1,11 +1,14 @@
 const dotenv=require("dotenv");
 const {join}=require("path");
 
-let env_absPath=join(__dirname,"../.env")
+
+let env_absPath=join(__dirname,`../envs/.env.${process.env.APP_ENV}`)
+
 dotenv.config({path:env_absPath});
 
 const APP_CONN_VARS={
    port:process.env.PORT
 }
+
 
 module.exports={APP_CONN_VARS};
